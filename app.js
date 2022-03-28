eef9ab28916aa62e886a0fe45466a48b4e6cf179
suyash159 <s.maheshwari@braininventory.com>
2022-03-28 18:34:02 +0530

    started working

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ dotenv.config();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static("build"));
 
 app.set("view engine", "jade");
 
@@ -29,11 +28,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("build"));
 
-app.use("/payslip", paySlipRouter);
-app.use("/user", usersRouter);
-app.use("/employee", employeeRouter);
-app.use("/salary", salaryRouter);
+app.use("/api/payslip", paySlipRouter);
+app.use("/api/user", usersRouter);
+app.use("/api/employee", employeeRouter);
+app.use("/api/salary", salaryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
